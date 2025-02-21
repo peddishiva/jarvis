01ab0e17dfad712873265c260dce4af33e29cd9b
peddishiva <peddishiva63@gmail.com>
2025-02-22 02:14:18 +0530

    Backend Connection

--- engine/command.py
+++ engine/command.py
@@ -1,33 +1,39 @@
 import pyttsx3
 import speech_recognition as sr
+import eel
 
 def speak(text):
-    engine = pyttsx3.init("sapi5")
-    voices = engine.getProperty('voices')
-    engine.setProperty('voice', voices[1].id) 
-    engine.setProperty('rate', 174) 
+    engine = pyttsx3.init('sapi5')
+    voices = engine.getProperty('voices') 
+    engine.setProperty('voice', voices[0].id)
+    engine.setProperty('rate', 174)
     print(voices)
     engine.say(text)
     engine.runAndWait()
 
+@eel.expose
 def takecommand():
-    
+
     r = sr.Recognizer()
-    
+
     with sr.Microphone() as source:
-        print("Listening...")
+        print('listening...')
+        eel.DisplayMessage('listening...')
         r.pause_threshold = 1
         r.adjust_for_ambient_noise(source)
         
         audio = r.listen(source, 10, 6)
-        
+
     try:
-        print("Recognizing...")
+        print('Recognizing...')
+        eel.DisplayMessage('Recognizing...')
         query = r.recognize_google(audio, language='en-in')
-        print(f"User said: {query}\n")
+        print(f"user said: {query}")
+        eel.DisplayMessage(query)
+        speak(query)
+        eel.ShowHood()
+        
     except Exception as e:
         return ""
-    return query.lower()
 
-text = takecommand()  
-speak(text)
+    return query.lower()
---
+++ www/controller.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+    // Display Speak Message
+    eel.expose(DisplayMessage)
+    function DisplayMessage(message) {
+
+        $(".siri-message li:first").text(message);
+        $('.siri-message').textillate('start');
+
+    }
+
+     // Display hood
+    eel.expose(ShowHood)
+    function ShowHood() {
+        $("#oval").attr("hidden", false);
+        $("#siriwave").attr("hidden", true);
+    }
+
+});
